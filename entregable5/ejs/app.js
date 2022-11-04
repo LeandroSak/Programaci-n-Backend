4bb3d077c9d1ae24a8d67a7085507ae443af20f2
LeandroSanchez <sanchezk.leandro@gmail.com>
2022-11-03 22:27:48 -0300

    entregable 5

---
+++ entregable5/ejs/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+require('dotenv').config();
+
+const cont=require("./storage/contenedor")
+const contenedor = new cont()
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (_req,res) =>{
+    let message = "tu mama"
+    res.render('pages/index', {message});
+})
+
+app.post('/productos', async(req,res)=>{
+    try{
+        await contenedor.save(req.body)
+        res.status(200).redirect('/')
+    }catch(error){
+        res.status(400).json({
+            response: "error",
+            error: error.message
+        })
+    }
+})
+
+app.get('/productos', async(_req,res)=>{
+    try{
+        const products = await contenedor.getAll()
+        res.render('pages/productos',{listProducts:products, listExist:true})
+    }catch(error){
+        res.status(400).json({
+            response: "error",
+            error: error.message
+        })
+    }
+})
+
+module.exports = app;
