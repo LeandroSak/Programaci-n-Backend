633133b26ae5c085c239ac55c11f29ec3cbf625f
LeandroSanchez <sanchezk.leandro@gmail.com>
2023-01-31 22:02:29 -0300

    entrega 12 Usando el objeto process

--- entregable11/server.js
+++ entregable11/server.js
@@ -1,6 +1,11 @@
 
 const http = require("./app")
+const minimist = require("minimist")
 
-const PORT = process.env.PORT || 4000
+
+const options = {alias:{p:"port"}}
+const data = minimist(process.argv.slice(2),options)
+const PORT = data.port || 8080
+
 
 http.listen(PORT, () => console.info(`Server up and running on port ${PORT}`))
